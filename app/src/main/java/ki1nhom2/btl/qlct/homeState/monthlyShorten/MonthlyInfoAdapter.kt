60238adb05fda9e57d146ebdd01fdaf67bd84442
Chí Hiếu <hieumeo171@gmail.com>
2022-10-24 18:02:37 +0700

    update home_state

--- app/src/main/java/ki1nhom2/btl/qlct/homeState/monthlyShorten/MonthlyInfoAdapter.kt
+++ app/src/main/java/ki1nhom2/btl/qlct/homeState/monthlyShorten/MonthlyInfoAdapter.kt
@@ -2,10 +2,16 @@ package ki1nhom2.btl.qlct.homeState.monthlyShorten
 
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.inflate
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
 import ki1nhom2.btl.qlct.R
+import ki1nhom2.btl.qlct.addState.AddStateActivity
+import ki1nhom2.btl.qlct.homeState.HomeStateActivity
 
 class MonthlyInfoAdapter(private val mList: List<MonthlyInfoNode>) : RecyclerView.Adapter<MonthlyInfoAdapter.ViewHolder>() {
 
@@ -13,7 +19,7 @@ class MonthlyInfoAdapter(private val mList: List<MonthlyInfoNode>) : RecyclerVie
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         // inflates the card_view_design view
         // that is used to hold list item
-        val view = LayoutInflater.from(parent.context)
+        val view : View = LayoutInflater.from(parent.context)
                 .inflate(R.layout.home_node_monthly_info, parent, false)
 
         return ViewHolder(view)
@@ -28,6 +34,31 @@ class MonthlyInfoAdapter(private val mList: List<MonthlyInfoNode>) : RecyclerVie
         holder.income.text = monthlyStatisticStructure.income.toString()
         holder.outcome.text = monthlyStatisticStructure.outcome.toString()
 
+        holder.dropDown.setOnClickListener {
+            generateConsumptionList(holder, monthlyStatisticStructure.monthName)
+            HomeStateActivity.adapter.notifyItemChanged(position)
+        }
+    }
+
+    private fun generateConsumptionList(holder : ViewHolder, monthName : String) {
+
+        val child : View = LayoutInflater.from(holder.dropDown.context).inflate(R.layout.home_consumption_details, null)
+        val name : TextView = child.findViewById(R.id.name)
+        val type : TextView = child.findViewById(R.id.type)
+        val cost : TextView = child.findViewById(R.id.cost)
+        val date : TextView = child.findViewById(R.id.date)
+        val btnSeeMore : MaterialButton = child.findViewById(R.id.btnSeeMore)
+
+        for(i in 0 until AddStateActivity.consumptionInfo.size) {
+            if(AddStateActivity.consumptionInfo[i].date.substring(3, 5) == monthName) {
+                name.text = AddStateActivity.consumptionInfo[i].name
+                type.text = AddStateActivity.consumptionInfo[i].type
+                cost.text = AddStateActivity.consumptionInfo[i].cost.toString()
+                date.text = AddStateActivity.consumptionInfo[i].date
+
+                holder.list.addView(child)
+            }
+        }
     }
 
     // return the number of the items in the list
@@ -40,5 +71,8 @@ class MonthlyInfoAdapter(private val mList: List<MonthlyInfoNode>) : RecyclerVie
         val monthName: TextView = itemView.findViewById(R.id.monthName)
         val income: TextView = itemView.findViewById(R.id.income)
         val outcome: TextView = itemView.findViewById(R.id.outcome)
+        val dropDown : ImageButton = itemView.findViewById(R.id.dropdown)
+        val list : LinearLayout = itemView.findViewById(R.id.consumptionList)
+
     }
 }
